f2bbaaf8f9a9a67c29d7a7dae6277ec0b5fa78e4
arsenkylysbek <arsen.kylyshbek@gmail.com>
2025-04-14 18:23:45 -0700

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  async headers() {
-    return [
-      {
-        source: "/(.*)", // applies to all routes
-        headers: [
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "Content-Security-Policy",
-            value: "frame-ancestors 'none';",
-          },
-        ],
-      },
-    ];
-  },
+  reactStrictMode: false
 };
 
 export default nextConfig;
